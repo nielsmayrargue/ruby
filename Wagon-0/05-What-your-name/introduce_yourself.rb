6567bafac75e1ed90862a7da1980b8c3712af736
Niels <niels.mayrargue@gmail.com>
2014-01-06 18:50:48 +0100

    solution exo 0005

--- Wagon-0/05-What-your-name/introduce_yourself.rb
+++ Wagon-0/05-What-your-name/introduce_yourself.rb
@@ -1,1 +1,9 @@
 # your code goes here, introduce yourself !
+puts 'what\'s your first name ?'
+first_name = gets.chomp
+puts 'What\'s your middle name ?'
+middle_name = gets.chomp
+puts 'What\'s your last name ?'
+last_name = gets.chomp
+puts "Hello, #{first_name} #{middle_name} #{last_name} !"
+puts 'Hello, ' + first_name + ' ' + middle_name + ' ' + last_name + ' !'
