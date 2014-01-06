0105b9082eb7519ab0ffe26d07fdb7f7935c9440
Niels <niels.mayrargue@gmail.com>
2014-01-06 18:26:08 +0100

    Solution 00 04 nettoyée

--- Wagon-0/04-Age-in-days/age_in_days.rb
+++ Wagon-0/04-Age-in-days/age_in_days.rb
@@ -1,10 +1,9 @@
-def age_in_days(day, month, year)
+def age(year, month, day)
 (Time.new - Time.new(year, month, day))/60/60/24
 end
 
 
 # Testing your code
-puts age_in_days(6, 11, 1985)
 puts age(1985, 6, 11)
 10436.7930035725
 => nil
