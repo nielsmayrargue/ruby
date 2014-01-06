42c6f09444b125cdf792b4b123c3f5b93dbc674c
Niels <niels.mayrargue@gmail.com>
2014-01-06 22:36:48 +0100

    solution 00 06 avec further

--- Wagon-0/06-Stupid-coaching/stupid_coaching.rb
+++ Wagon-0/06-Stupid-coaching/stupid_coaching.rb
@@ -1,6 +1,30 @@
 def stupid_coaching 
-  # your code goes here
+puts 'What\'s your question ?'
+answer = gets.chomp
+if answer[-1,1] == "?"
+	puts 'Silly question, get dressed and go to work !'
+elsif answer[-1,1] == "!"
+	puts 'Amazing Niels ! I will answer all your questions !'
+elsif answer == "I am going to work"
+new_rela
+elsif answer.upcase! == nil
+puts 'I can feel your motivation son !'
+else
+	puts 'I don\'t care son, get dressed and go to work'
+end
+stupid_coaching
 end
 
+def new_rela
+puts 'what\'s your question ?'
+answer_2 = gets.chomp
+if answer_2[-1,1] == "?"
+puts 'Silly question, take the subway and go to work !'
+elsif answer_2[-1,1] == "!"
+puts 'I don\'t care son, take the subway and go to work !'
+end
+new_rela
+end
+	
 # Calling the method
 stupid_coaching
