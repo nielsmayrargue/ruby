ac08648ca3e92d40e939833651d95bd278f35f33
Niels <niels.mayrargue@gmail.com>
2014-01-07 19:41:39 +0100

    solution 0102

--- Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
+++ Wagon-1/02-Sorting-the-wagon/wagon_sort.rb
@@ -1,5 +1,21 @@
-def wagon_sort( students_array )
-  # your code to sort the array and output students
+def wagon_sort
+student_array = []
+puts 'Type a student:'
+name = gets.chomp
+
+	until name.empty?
+		add_student(student_array,name)
+		puts 'Type a student:'
+		name = gets.chomp	
+	end
+
+puts student_array
+
 end
 
+def add_student(student_array,name)
+	student_array << name
+	student_array.sort_by! { |x| x.downcase }
+end
 # your code to interact with the user..
+wagon_sort
