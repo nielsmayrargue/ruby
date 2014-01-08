4244ebfea3dde0949e85815d43ebfca6259c397b
Niels <niels.mayrargue@gmail.com>
2014-01-08 15:06:13 +0100

    solution 0105

--- Wagon-1/05-Roman-numerals/roman_numerals.rb
+++ Wagon-1/05-Roman-numerals/roman_numerals.rb
@@ -1,10 +1,58 @@
-def old_roman_numeral(an_integer)
-  # your code goese here
+def old_roman_numeral(integer)
+conversion_old = [ 
+	["M",1000],
+	["D",500],
+	["C",100],
+	["L",50],
+	["X",10],
+	["V",5],
+	["I",1]
+]
+
+result = ""
+r = integer # reste des divisions euclidiennes successives
+
+for i in 0..6
+	result << conversion_old[i][0]*(r / conversion_old[i][1])
+	r = (integer % conversion_old[i][1])
 end
 
-def roman_numeral(an_integer)
-  # your code goese here for the new-style version
+puts result
+
+end
+
+def new_roman_numeral(integer)
+conversion_new = [ 
+	["M",1000],
+	["CM",900],
+	["D",500],
+	["CD",400],
+	["C",100],
+	["XC",90],
+	["L",50],
+	["XL",40],
+	["X",10],
+	["IX",9],
+	["V",5],
+	["IV",4],
+	["I",1]
+]
+
+result = ""
+r = integer # reste des divisions euclidiennes successives
+
+for i in 0..12
+	result << conversion_new[i][0]*(r / conversion_new[i][1])
+	r = (integer % conversion_new[i][1])
+end
+
+puts result
+
 end
 
 puts "My nice roman numeral tests" 
 # Write a nice testing script herebelow !
+puts 'write an arabic number'
+integer = gets.chomp.to_i
+puts "voici le resultat en ancien : #{old_roman_numeral(integer)}"
+puts "voici le resultat en nouveau : #{new_roman_numeral(integer)}"
