45ae7ff5443ba869961db612a8d58f593035c07d
Niels <niels.mayrargue@gmail.com>
2014-01-08 11:44:21 +0100

    solution 0104

--- Wagon-1/04-Cookbook-ToC/cookbook_toc.rb
+++ Wagon-1/04-Cookbook-ToC/cookbook_toc.rb
@@ -1,11 +1,21 @@
-title = 'Recettes de kiffeur'
+def edition
+	title = "Recettes de kiffeur"
+	chapters = [
+	             ['Materiel pour cuisiner', 1],
+	             ['Produits de saison', 8],
+	             ['Sauces et soupes', 19],
+	             ['Viandes et Gibiers', 38],
+	             ['Poissons, coquillages et crustaces', 157]
+	           ]
 
-chapters = [
-             ['Materiel pour cuisiner', 1],
-             ['Produits de saison', 8],
-             ['Sauces et soupes', 19],
-             ['Viandes et Gibiers', 38],
-             ['Poissons, coquillages et crustaces', 157]
-           ]
-           
-# You program goes here !
+	puts "#{title.center(70)}"
+	puts " "
+	puts " "
+
+	chapters.each do |x| 
+			puts "#{x[0].rjust(0)} #{x[1].to_s.rjust(78-x[0].length)}"
+	end
+
+end
+
+edition 
