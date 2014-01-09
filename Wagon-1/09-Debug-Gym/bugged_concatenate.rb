e6f59b2bfda83f4ca652e3bcbb9a75e1c0b41e8b
Niels <niels.mayrargue@gmail.com>
2014-01-09 12:23:30 +0100

    solution advanced 0109

--- Wagon-1/09-Debug-Gym/bugged_concatenate.rb
+++ Wagon-1/09-Debug-Gym/bugged_concatenate.rb
@@ -1,8 +1,12 @@
-def bugged_concatenate(array)
-  # 2. fix the code: TypeError
-  puts array.inject("") { |output, element| output + element }.upcase
-end
+def bugged_concatenate(*args)
+  # 2. fix the code: TypeError;
+  args = args[0] if args.length == 1 and args[0].is_a?(Array)
+	puts args.inject("") { |output, element| output + element.to_s }.upcase
+ end
 
 
 # 1. fix the method call: ArgumentError
+
+bugged_concatenate( [1, "9", 84, " ", "George Orwell"] )
+
 bugged_concatenate( 1, "9", 84, " ", "George Orwell" )
