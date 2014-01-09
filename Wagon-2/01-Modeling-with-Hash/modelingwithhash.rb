d45b7afdd2fa0860f66013a7f1f23281cc9c250c
Niels <niels.mayrargue@gmail.com>
2014-01-09 17:37:10 +0100

    solu progress 0201

---
+++ Wagon-2/01-Modeling-with-Hash/modelingwithhash.rb
@@ -0,0 +1,52 @@
+def calories_in(*meal)
+	$conversion = [
+		["Cheese_Burger", 290],
+		["Big_Mac", 300],
+		["Mc_Bacon", 300],
+		["Royal_Cheese", 130],
+		["French_fries", 130],
+		["Potatoes", 130],
+		["Coca", 160],
+		["Sprite", 170],
+	]
+  
+  calories = 0
+  meal.each do |x|
+    $conversion.each do |y|
+      if x == y[0]
+  		  calories += y[1]
+  		end
+  	end
+  end
+
+ puts calories
+end 
+
+def calories_in_hash(*meal)
+  $conversion_hash = Hash.new
+  {
+    "Cheese_Burger" => 290,
+    "Big_Mac" => 300,
+    "Mc_Bacon" => 300,
+    "Royal_Cheese" => 130,
+    "French_fries" => 130,
+    "Potatoes" => 130,
+    "Coca" => 160,
+    "Spite" => 170
+  }
+
+
+meal.each { |x| puts $conversion_hash[x] }
+
+end
+
+#test
+calories_in("Cheese_Burger", "Coca")
+
+#update with filet_of_fish
+$conversion << ["filet_of_fish", 100]
+puts $conversion[8]
+
+#Call hash
+calories_in_hash("Cheese_burger", "Coca")
+
