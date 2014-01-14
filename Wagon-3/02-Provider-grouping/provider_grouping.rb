5be52b98a0e7d628fa2db7e4e1ae19c6b26a0079
Niels <niels.mayrargue@gmail.com>
2014-01-14 16:13:04 +0100

    solution 0302

--- Wagon-3/02-Provider-grouping/provider_grouping.rb
+++ Wagon-3/02-Provider-grouping/provider_grouping.rb
@@ -1,5 +1,32 @@
 def group_mails( emails ) 
-  # your code goes here
+repertory = {}
+
+emails.each do |email|
+	provider = email[email.index("@")+1...email.index(".")]
+  #provider = email.match(/@[\w]+/).to_s[1..-1]
+	repertory[provider] != nil ? repertory[provider] << email  : repertory[provider] = [email]
+end
+
+return repertory
+
+end
+
+def has_provider?(user_array, provider)
+
+  answer = false
+
+  user_array.each do |user|
+    provider_user = user.split("@").last
+    answer = true if provider_user.include?(provider)
+      end
+
+  return answer
+end
+
+def has_provider2(user_array,provider)
+
+  return group_mails(user_array).include?(provider)
+
 end
 
 users_emails = %w( bob@yahoo.fr 
@@ -11,6 +38,8 @@ users_emails = %w( bob@yahoo.fr
                    
 
 puts group_mails( users_emails ) 
+puts has_provider?(users_emails, "yahoo")
+puts has_provider2(users_emails, "lala")
 
 # => {"yahoo" => ["bob@yahoo.fr", "bigbox@yahoo.fr"], 
 #     "hotmail" => ["roger57@hotmail.fr"], 
