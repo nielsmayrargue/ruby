6303ae2301722205f859449f16116a06ad8b12b5
Niels <niels.mayrargue@gmail.com>
2014-01-15 15:26:25 +0100

    solution 0305

--- Wagon-3/05-csv-parsing/parsing_recipes.rb
+++ Wagon-3/05-csv-parsing/parsing_recipes.rb
@@ -1,59 +1,52 @@
+# Main function : run the whole program 
 def cookbook(csv_file)
 
   require 'csv'
 
-  mega_hash = {}
+  recipe_dictionary = {}
 
-  ordered_row = [1,0,3,2,4]
-
+# The CSV file is converted into a hash in order to easily access every information
   CSV.foreach(csv_file) do |row|
-
-    
-    #ordered_row.each { |x| row << row[x] }
-
-    
-    #row.delete_at(0)
-    #row.delete_at(1)
-    #row.delete_at(2)
-    #row.delete_at(3)
-    #row.delete_at(4)
-    
-    if mega_hash[row[1]] == nil 
-    	mega_hash[row[1]] = {} 
+     
+    if recipe_dictionary[row[1]] == nil 
+    	recipe_dictionary[row[1]] = {} 
     end
 
-    mega_hash[row[1]][row[0]] = {}
-    mega_hash[row[1]][row[0]]["difficulty"] = row[3]
-    mega_hash[row[1]][row[0]]["prep"] = row[2]
-    mega_hash[row[1]][row[0]]["cook"] = row[4]
+    recipe_dictionary[row[1]][row[0]] = {}
+    recipe_dictionary[row[1]][row[0]]["prep"] = row[2]
+    recipe_dictionary[row[1]][row[0]]["cook"] = row[4]
+    # The difficulty of each recipe is valuated according to a defined range
+    if row[3].to_i > 100
+     recipe_dictionary[row[1]][row[0]]["difficulty"] = "difficile"
+    elsif row[3].to_i < 100 && row[3].to_i > 60
+     recipe_dictionary[row[1]][row[0]]["difficulty"] = "moyen"
+    else
+    recipe_dictionary[row[1]][row[0]]["difficulty"] = "facile"
+    end
 
   end
-
-  def affichage(array)
-    array.each { |x| puts x }
-  end
-  
+ 
   puts "We have recipes for :"
-  puts "- Entree"
-  puts "- Plat Principal"
-  puts "- Dessert"
-  puts "- Sauce"
-
+  recipe_dictionary.keys.each { |x| puts "-#{x}" } 
+ 
   puts "Which category are you interested in ?"
 
   type = gets.chomp
+  
+  puts "\n" + "Here are the recipes of the category :" + "\n"
+  recipe_dictionary[type].keys.each { |x| puts "-#{x}" }
 
-  puts "Here are the recipes of the category : #{affichage(mega_hash[type].keys)}"
-
-  puts "Now which recipe ?"
+  puts " "
+  puts "\n" + "Now which recipe ?"
 
   recipe = gets.chomp
 
-  puts recipe
+  puts " "
+  puts "\n" + "recipe"
   puts "************************************"
-  puts "difficulty : #{mega_hash[type][recipe]["difficulty"]}"
-  puts "preparation  : #{mega_hash[type][recipe]["prep"]}min"
-  puts "cooking  : #{mega_hash[type][recipe]["cook"]}h"
+  puts "difficulty : #{recipe_dictionary[type][recipe]["difficulty"]}"
+  puts "preparation  : #{recipe_dictionary[type][recipe]["prep"]}min"
+  puts "cooking  : #{recipe_dictionary[type][recipe]["cook"]}h"
 
 end
 
