456fddc36b9951531775c429ebd1c21229403c11
Niels <niels.mayrargue@gmail.com>
2014-01-15 13:21:01 +0100

    wip 0305

--- Wagon-3/05-csv-parsing/parsing_recipes.rb
+++ Wagon-3/05-csv-parsing/parsing_recipes.rb
@@ -1,11 +1,61 @@
-require 'csv'
+def cookbook(csv_file)
 
-mega_hash = {}
+  require 'csv'
 
+  mega_hash = {}
 
-CSV.foreach("recipes.csv") do |row|
-	def mise_en_hash 
+  ordered_row = [1,0,3,2,4]
 
-	end
+  CSV.foreach(csv_file) do |row|
 
-puts mega_hash
+    
+    #ordered_row.each { |x| row << row[x] }
+
+    
+    #row.delete_at(0)
+    #row.delete_at(1)
+    #row.delete_at(2)
+    #row.delete_at(3)
+    #row.delete_at(4)
+    
+    if mega_hash[row[1]] == nil 
+    	mega_hash[row[1]] = {} 
+    end
+
+    mega_hash[row[1]][row[0]] = {}
+    mega_hash[row[1]][row[0]]["difficulty"] = row[3]
+    mega_hash[row[1]][row[0]]["prep"] = row[2]
+    mega_hash[row[1]][row[0]]["cook"] = row[4]
+
+  end
+
+  def affichage(array)
+    array.each { |x| puts x }
+  end
+  
+  puts "We have recipes for :"
+  puts "- Entree"
+  puts "- Plat Principal"
+  puts "- Dessert"
+  puts "- Sauce"
+
+  puts "Which category are you interested in ?"
+
+  type = gets.chomp
+
+  puts "Here are the recipes of the category : #{affichage(mega_hash[type].keys)}"
+
+  puts "Now which recipe ?"
+
+  recipe = gets.chomp
+
+  puts recipe
+  puts "************************************"
+  puts "difficulty : #{mega_hash[type][recipe]["difficulty"]}"
+  puts "preparation  : #{mega_hash[type][recipe]["prep"]}min"
+  puts "cooking  : #{mega_hash[type][recipe]["cook"]}h"
+
+end
+
+cookbook("recipes.csv")
+
