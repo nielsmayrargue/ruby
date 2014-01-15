8019cac64484fc70744f0080059de45a90d16938
Niels <niels.mayrargue@gmail.com>
2014-01-15 17:02:13 +0100

    solution 0307 with addon

--- Wagon-3/07-Extend-built-in-classes/extend_built-in.rb
+++ Wagon-3/07-Extend-built-in-classes/extend_built-in.rb
@@ -1,9 +1,11 @@
+# Method that leads to factorial outside of a class
 def factorial(an_integer)
 	result = 1
 	(1..an_integer).each { |x| result*= x }
 	return result
 end
 
+# Adding the factorial method to the Fixnum class
 class Fixnum
 	def factorial
     result = 1
@@ -12,6 +14,16 @@ class Fixnum
   end
 end
 
+#Adding a shuffle method to the Array class
+
+class Array
+  def shuffle_newnew(n)
+    self[0,(n+1)].shuffle
+  end
+end
+
 #test
 puts factorial(6)
 puts 6.factorial
+array = [1,"wagon","irma",3,"robert"]
+print array.shuffle_newnew(3)
